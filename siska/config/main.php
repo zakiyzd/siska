c9ee0f30c65c03c57374cb83239919c1a95f7906
zakiyzd <zakiyazid10@gmail.com>
2024-05-20 14:16:59 +0700

    Membuat aplikasi SISKA

---
+++ siska/config/main.php
@@ -0,0 +1,50 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-siska',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'siska\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-siska',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-siska', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the siska
+            'name' => 'advanced-siska',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
